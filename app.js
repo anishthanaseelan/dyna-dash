ed896b5dfd8def7796f4e79b63841aed1c293445
Anish Thanaseelan Kaspaar <Anish Thanaseelan Kaspaar>
2019-10-11 05:03:50 +0100

    Added layout and properties

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
+init()
+var maxwidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var cellsize = (maxwidth - 5) / properties.settings.grid.maxx;
 drawSkaliton()
 
+function init() {
+
+}
 function drawSkaliton() {
-    // 
     var htmltxt = ""
-    layout.forEach(element => {
-        if (element.type == "text") {
-            htmltxt += "<div class=\"chartBox\"><dev id=\"" + element.id + "\" class=\"textchart\"></div></div >"
+    properties.layout.forEach(element => {
+        if (element.type == "title") {
+            htmltxt += "<div id=\"" + element.id + "\" class=\"title\"></div >"
+        } else if (element.type == "text") {
+            htmltxt += "<div class=\"chartBox\"><div class=\"chartTitle\">" + element.title + "</div><dev id=\"" + element.id + "\" class=\"textData\"></div></div >"
         } else {
-            htmltxt += "<div class=\"chartBox\"><canvas id=\"" + element.id + "\" class=\"chart\"></canvas></div >"
+            htmltxt += "<div class=\"chartBox\"><div class=\"chartTitle\">" + element.title + "</div><div><canvas id=\"" + element.id + "\" class=\"chart\"></canvas></div></div >"
         }
 
     });
-    //    console.log(htmltxt)
     var dash = document.getElementById("dashboard").innerHTML = htmltxt
-    layout.forEach(element => {
-        if (element.type == "text") {
+    properties.layout.forEach(element => {
+        if (element.type == "title") {
+            drawTitle(element, element.title)
+        } else if (element.type == "text") {
             drawText(element, finddata(element.id))
         } else {
             drawChart(element, finddata(element.id))
         }
     });
 }
+
 function finddata(id) {
     var data;
     reportData.forEach(element => {
@@ -32,17 +41,36 @@ function finddata(id) {
     });
     return data;
 }
+
 function drawText(layoutData, chartData) {
     var ctx = document.getElementById(layoutData.id);
-    ctx.innerHTML = "<div class = \"textTitle\">" + layoutData.title + "</div><div class = \"textData\">" + chartData + " </div>";
-    ctx.parentNode.style.height = layoutData.height;
-    ctx.parentNode.style.width = layoutData.width;
+    ctx.innerHTML = chartData;
+    //    ctx.innerHTML = "<div class = \"textData\">" + chartData + " </div>";
+    ctx.style.height = (layoutData.height * cellsize) - 30 - 20 + "px";
+    ctx.style.width = (layoutData.width * cellsize) - 20 + "px";
+    ctx.parentNode.style.top = layoutData.y * cellsize + "px";
+    ctx.parentNode.style.left = layoutData.x * cellsize + "px";
+}
+
+function drawTitle(layoutData, chartData) {
+    console.log(layoutData.id)
+    var ctx = document.getElementById(layoutData.id);
+    ctx.innerHTML = "<div class = \"titletext\">" + layoutData.title + "</div>";
+    ctx.style.height = (layoutData.height * cellsize) - 10 + "px";
+    ctx.style.width = (layoutData.width * cellsize) - 10 + "px";
+    ctx.style.top = layoutData.y * cellsize + "px";
+    ctx.style.left = layoutData.x * cellsize + "px";
 }
 
 function drawChart(layoutData, chartData) {
     var ctx = document.getElementById(layoutData.id).getContext('2d');
-    ctx.canvas.parentNode.style.height = layoutData.height;
-    ctx.canvas.parentNode.style.width = layoutData.width;
+    //Margin correction - 5 + 5
+    //Padding correction - 5 + 5 
+    //Title higtht corrections - 30 
+    ctx.canvas.parentNode.style.height = (layoutData.height * cellsize) - 30 - 20 + "px";
+    ctx.canvas.parentNode.style.width = (layoutData.width * cellsize) - 20 + "px";
+    ctx.canvas.parentNode.parentNode.style.top = layoutData.y * cellsize + "px"
+    ctx.canvas.parentNode.parentNode.style.left = layoutData.x * cellsize + "px";
     var myChart = new Chart(ctx, {
         type: layoutData.type,
         data: {
@@ -75,13 +103,14 @@ function drawChart(layoutData, chartData) {
                 display: false,
             },
             title: {
-                display: true,
+                display: false,
                 text: layoutData.title
             },
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero: true
+                        display: false,
+                        beginAtZero: false
                     }
                 }]
             }
